84f0a95192d868fae051956c3daa05e936555c5b
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-07-12 15:18:20 +0300

    add tests

---
+++ test/http_test.asm
@@ -0,0 +1,113 @@
+format ELF64
+
+include "include/c.inc"
+include "include/http.inc"
+
+section '.data' writeable
+
+drop_sp_test_fmt:
+    db "  Running drop_sp_test...", 10, 0
+parse_method_test_fmt:
+    db "  Running parse_method_test...", 10, 0
+parse_request_uri_test_fmt:
+    db "  Running parse_request_uri_test...", 10, 0
+
+drop_sp_test_failed_fmt:
+    db "    Dropped %d spaces instead of 5", 10, 0
+parse_failed_fmt:
+    db "    Parsed %d characters, but expect %d", 10, 0
+
+drop_sp_test_data:
+    db "     khooy", 0
+parse_method_test_data:
+    db "GET khooy"
+parse_request_uri_test_data:
+    db "/khooy/test?arg1=foo&arg2=bar  fsdjkf", 10, 0
+
+section '.text'
+
+public drop_sp_test
+public parse_method_test
+public parse_request_uri_test
+
+drop_sp_test:
+    push rbp
+
+    mov rdi, drop_sp_test_fmt
+    call printf
+
+    mov rdi, drop_sp_test_data
+    call drop_sp
+
+    cmp rax, drop_sp_test_data + 5
+    je .passed
+
+    mov rdi, drop_sp_test_failed_fmt
+    mov rsi, rax
+    sub rsi, drop_sp_test_data
+    call printf
+
+    pop rbp
+    mov rax, 1
+    ret
+
+.passed:
+    pop rbp
+    mov rax, 0
+    ret
+
+parse_method_test:
+    push rbp
+
+    mov rdi, parse_method_test_fmt
+    call printf
+
+    mov rdi, parse_method_test_data
+    call parse_method
+
+    cmp rax, parse_method_test_data + 3
+    je .passed
+
+    mov rdi, 2
+    mov rsi, parse_failed_fmt
+    mov rdx, rax
+    sub rdx, parse_method_test_data
+    mov rcx, 3
+    call dprintf
+
+    pop rbp
+    mov rax, 1
+    ret
+
+.passed:
+    pop rbp
+    mov rax, 0
+    ret
+
+parse_request_uri_test:
+    push rbp
+
+    mov rdi, parse_request_uri_test_fmt
+    call printf
+
+    mov rdi, parse_request_uri_test_data
+    call parse_request_uri
+
+    cmp rax, parse_request_uri_test_data + 29
+    je .passed
+
+    mov rdi, 2
+    mov rsi, parse_failed_fmt
+    mov rdx, rax
+    sub rdx, parse_request_uri_test_data
+    mov rcx, 29
+    call dprintf
+
+    pop rbp
+    mov rax, 1
+    ret
+
+.passed:
+    pop rbp
+    mov rax, 0
+    ret
---
+++ test/test.asm
@@ -0,0 +1,61 @@
+format ELF64
+
+include "include/c.inc"
+include "http_test.inc"
+define EXPECTED_TEST_COUNT 3
+
+section '.data' writeable
+
+splash_fmt:
+    db "Running unit tests...", 10, 0
+passed_tests_fmt:
+    db "Passed tests: %d/%d", 10, 0
+
+section '.bss' writeable
+counter: rq 1
+
+section '.text'
+
+public main
+
+main:
+    push rbp
+
+    mov rdi, splash_fmt
+    mov rax, 0
+    call printf
+
+    call drop_sp_test
+    cmp rax, 0
+    jne .end
+    inc qword [counter]
+
+    call parse_method_test
+    cmp rax, 0
+    jne .end
+    inc qword [counter]
+
+    call parse_request_uri_test
+    cmp rax, 0
+    jne .end
+    inc qword [counter]
+
+.end:
+
+    mov rdi, passed_tests_fmt
+    mov rsi, [counter]
+    mov rdx, EXPECTED_TEST_COUNT
+    mov rax, 0
+    call printf
+
+    cmp qword [counter], EXPECTED_TEST_COUNT
+    je .passed
+
+    pop rbp
+    mov rax, 1
+    ret
+
+.passed:
+    pop rbp
+    mov rax, 0
+    ret
